80c188b194ec51f11cef404202656a03c0558c86
Everton Zoelner <everton.zoelner@gmail.com>
2019-07-18 00:09:47 -0300

    Configurando Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { ReactComponent as Logo } from "~/assets/logo.svg";
+
+import { Container, Content, Profile } from "./styles";
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <span>
+            <Logo />
+          </span>
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Everton Zoelner</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Everton Zoelner"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
